35179af50cb663a0c4c6078367bfe9d9e5e65183
Akridev <87054351+Akridev@users.noreply.github.com>
2021-07-15 18:19:36 +0800

    Update mad.js

--- commands/mad.js
+++ commands/mad.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'mad',
     description: 'returns id of mentioned user',
     execute(message, args) {
-        message.channel.send('Oi that\'s proper mad blud')
+        message.channel.send('Oi that\'s proper mad bludd')
     }
 }
